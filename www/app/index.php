3d0b10ad75d899a4fe154b8054cd8510e17c3892
Dave Slack <davidkslack@yahoo.co.uk>
2020-01-16 13:09:53 +0000

    Changes to the password on the home page

--- www/app/index.php
+++ www/app/index.php
@@ -17,7 +17,7 @@
         <li>PHP <?= phpversion(); ?></li>
         <li>
             <?php
-            $link = mysqli_connect("mysql", "root", "tiger", null);
+            $link = mysqli_connect("mysql", "root", "rootpass", null);
 
             /* check connection */
             if (mysqli_connect_errno()) {
